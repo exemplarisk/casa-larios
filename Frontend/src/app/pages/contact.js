a2b009a89db0021704525b7588af42093150e768
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-04-10 17:58:39 +0200

    ContactForm

--- Frontend/src/app/pages/contact.js
+++ Frontend/src/app/pages/contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ContactForm from '../components/ContactForm'
 
 function Contact() {
 
@@ -6,31 +7,8 @@ function Contact() {
 
     return (
         <div>
-            <div style={{ display: 'flex', flexDirection: "column", textAlign: "center" }}>
-                <h1 style={{ fontSize: "36px" }}>Contact</h1>
-                <div>
-                    <div style={{ marginTop: "10vh" }}>
-                        <button type="button" className=" max-w-xl top-1/2 left-1/2 text-white bg-gradient-to-r
-                            from-purple-500
-                            via-purple-600
-                            to-purple-700
-                            hover:bg-gradient-to-br
-                            focus:ring-4
-                            focus:outline-none
-                            focus:ring-purple-300
-                            dark:focus:ring-purple-800
-                            font-medium rounded-lg
-                            text-sm px-5 py-2.5
-                            text-center mr-2 mb-2">
-                            <a href={mailtoHref} className="btn btn-danger">
-                                Send email
-                            </a>
-                        </button>
-                    </div>
-                </div>
-            </div>
+            <ContactForm />
         </div>
-        
     )
 }
 
