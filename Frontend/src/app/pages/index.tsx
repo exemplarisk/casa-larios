33dbfd8868860f6abf9b1a97be40893d11f01c57
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-04-11 18:18:48 +0200

    Added welcome message take a look

--- Frontend/src/app/pages/index.tsx
+++ Frontend/src/app/pages/index.tsx
@@ -39,6 +39,9 @@ const Home: NextPage = () => {
             <FrontInfo />
           <div style={{ textAlign: "center" }}>
             <br/>
+            <div>
+              <h1 className='text-6xl font-normal leading-normal mt-0 mb-2 text-pink-800'>Take a look around the corner</h1>
+            </div>
             <div id="swal">
               <button type="button" onClick={triggerSwal} className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">WELCOME</button>
             </div>
