bd8749ece81ad6b554a8eb880685b72914931cd9
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-04-11 15:41:29 +0200

    Images updated namings

--- Frontend/src/app/components/BlurImage.tsx
+++ Frontend/src/app/components/BlurImage.tsx
@@ -35,7 +35,6 @@ function BlurImage({ image }: { image: ImageProps }) {
                     onLoadingComplete={() => setLoading(false)}
                 />
             </div>
-            <h3 className="mt-4 text-sm text-gray-700">{image.username}</h3>
             <p className="mt-1 text-lg font-medium text-gray-900">{image.name}</p>
         </a>
     );
--- Frontend/src/app/components/helpers/loadImages.tsx
+++ Frontend/src/app/components/helpers/loadImages.tsx
@@ -6,168 +6,168 @@ export const images = [
         href: '/#',
         imageSrc: "/images/apartment/terrace33.jpg",
         name: "Terrace",
-        username: "terrace",
+        
     },
     {
         id: 2,
         href: '/#',
         imageSrc: "/images/apartment/terrace44.jpg",
         name: "Terrace",
-        username: "terrace",
+        
     },
     {
         id: 3,
         href: '/#',
         imageSrc: "/images/apartment/terrace.jpg",
         name: "Terrace",
-        username: "terrace",
+        
     },
     {
         id: 4,
         href: '/#',
         imageSrc: "/images/apartment/terracetable.jpg",
         name: "Terrace",
-        username: "terrace",
+        
     },
     {
         id: 5,
         href: '/#',
         imageSrc: "/images/apartment/terracesofa.jpg",
         name: "Terrace",
-        username: "terrace",
+        
     },
     {
         id: 6,
         href: '/#',
-        imageSrc: "/images/apartment/livingroom.jpg",
+        imageSrc: "/images/apartment/terraceside.jpg",
         name: "Terrace",
-        username: "terrace",
+        
     },
     {
         id: 7,
         href: '/#',
-        imageSrc: "/images/apartment/livingroomtable.jpg",
-        name: "Terrace",
-        username: "terrace",
+        imageSrc: "/images/apartment/hallwayl2b.jpg",
+        name: "Hallway",
+        
     },
     {
         id: 8,
         href: '/#',
-        imageSrc: "/images/apartment/kitchenhw.jpg",
-        name: "Terrace",
-        username: "terrace",
+        imageSrc: "/images/apartment/bathroom1.jpg",
+        name: "Bathroom",
+        
     },
     {
         id: 9,
         href: '/#',
-        imageSrc: "/images/apartment/bathroom1.jpg",
-        name: "Terrace",
-        username: "terrace",
+        imageSrc: "/images/apartment/bedroomdb.jpg",
+        name: "Bedroom",
+
     },
     {
         id: 10,
         href: '/#',
         imageSrc: "/images/apartment/bedroom2xdb.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Bedroom",
+
     },
     {
         id: 11,
         href: '/#',
-        imageSrc: "/images/apartment/bedroomdb.jpg",
-        name: "Terrace",
-        username: "terrace",
+        imageSrc: "/images/apartment/masterbedroom3.jpg",
+        name: "Master Bedroom",
+        
     },
     {
         id: 12,
         href: '/#',
         imageSrc: "/images/apartment/masterbroom22.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Master Bedroom",
+        
     },
     {
         id: 13,
         href: '/#',
         imageSrc: "/images/apartment/mbathroom2.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Master Bathroom",
+        
     },
     {
         id: 14,
         href: '/#',
         imageSrc: "/images/apartment/mbathroom3.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Master Bathroom",
+        
     },
     {
         id: 15,
         href: '/#',
         imageSrc: "/images/apartment/mbedroom33.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Master Bedroom",
+        
     },
     {
         id: 16,
         href: '/#',
-        imageSrc: "/images/apartment/hallwayl2b.jpg",
-        name: "Terrace",
-        username: "terrace",
+        imageSrc: "/images/apartment/masterbedroom4.jpg",
+        name: "Master Bedroom",
+        
     },
     {
         id: 17,
         href: '/#',
         imageSrc: "/images/apartment/hw2kit.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Kitchen",
+        
     },
     {
         id: 18,
         href: '/#',
         imageSrc: "/images/apartment/kitchenhw.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Kitchen",
+        
     },
     {
         id: 19,
         href: '/#',
         imageSrc: "/images/garden/aparoutb.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Pool",
+        
     },
     {
         id: 20,
         href: '/#',
         imageSrc: "/images/garden/aparoutc.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Pool",
+        
     },
     {
         id: 21,
         href: '/#',
         imageSrc: "/images/garden/aparoutd.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Pool",
+        
     },
     {
         id: 22,
         href: '/#',
         imageSrc: "/images/garden/apartaoutb.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Pool",
+        
     },
     {
         id: 23,
         href: '/#',
         imageSrc: "/images/garden/apartouta.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Pool",
+        
     },
     {
         id: 24,
         href: '/#',
         imageSrc: "/images/garden/apartoutc.jpg",
-        name: "Terrace",
-        username: "terrace",
+        name: "Pool",
+        
     }
 ]
 
