6a345e966ee81c4b15d5a216a7ad1be7f37ed2e5
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-04-22 22:38:45 +0200

    border around map & streetview

--- Frontend/src/app/components/MapView.tsx
+++ Frontend/src/app/components/MapView.tsx
@@ -4,7 +4,7 @@ function MapView() {
     return (
         <div id="mapdiv">
             <div id="position" style={{ border: 0, marginTop: "8px", height: "300px" }} >
-                <iframe style={{ width: "100%" }} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3146.548187631296!2d-0.7429988846775689!3d37.940984279729584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6307ceef5fb59d%3A0x66c4257a68279c08!2sCalle%20Guapiles%2C%207%2C%2003189%20Orihuela%2C%20Alicante%2C%20Spanien!5e0!3m2!1ssv!2sse!4v1625486117153!5m2!1ssv!2sse"
+                <iframe style={{ border: "5px solid #555", width: "100%" }} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3146.548187631296!2d-0.7429988846775689!3d37.940984279729584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6307ceef5fb59d%3A0x66c4257a68279c08!2sCalle%20Guapiles%2C%207%2C%2003189%20Orihuela%2C%20Alicante%2C%20Spanien!5e0!3m2!1ssv!2sse!4v1625486117153!5m2!1ssv!2sse"
                     width="600"
                     height="550"
                     aria-hidden="false"
--- Frontend/src/app/components/helpers/initStreetView.tsx
+++ Frontend/src/app/components/helpers/initStreetView.tsx
@@ -3,7 +3,7 @@ export const initStreetView = (url: string) => {
     return (
         <div id="streetdiv">
             <div id="position" style={{ display: "flex", justifyContent: "center", border: 0, marginBottom: "8px", height: "300px" }} >
-                <iframe style={{ width: "100%", }} src={url}
+                <iframe style={{ border: "5px solid #555", width: "100%", }} src={url}
                     width="600"
                     height="450"
                     aria-hidden="false"
