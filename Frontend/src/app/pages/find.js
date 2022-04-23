b38d9328fb32265bbb64f74c7551a8ab891d038e
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-04-23 09:22:47 +0200

    Fixes to maps

--- Frontend/src/app/pages/find.js
+++ Frontend/src/app/pages/find.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MapView from '../components/MapView'
-import Street from '../components/Street'
+import { initStreetView } from '../components/helpers/initStreetView'
 import Footer from '../components/Footer'
 
 
@@ -11,7 +11,7 @@ function Find() {
             <h1 className="mt-2 text-3xl leading-8 font-semibold tracking-tight text-indigo-600 sm:text-4xl" style={{ padding: "2rem" }}>FIND US HERE</h1>
             <div style={{ display: 'grid' }}>
                 <div id="streetview" style={{ marginBottom: "2vh", paddingLeft: "10vw", paddingRight: "10vw" }}>
-                    <Street />
+                    {initStreetView("https://www.google.com/maps/embed?pb=!4v1649490963317!6m8!1m7!1s8QgD1Sf6Zpfa1BGDe2S18Q!2m2!1d37.94108383284717!2d-0.74053230303486!3f255.24098!4f0!5f0.7820865974627469")}
                 </div>
                 <div id="mapview" style={{ marginTop: "10vh", paddingLeft: "10vw", paddingRight: "10vw" }}>
                     <MapView />
