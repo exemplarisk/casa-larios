a1139940313fbcc3a9fa9d4f72728e8af50b83b5
Jonatan Schultz <jonatan.schultz@blaklader.com>
2022-04-23 09:58:10 +0200

    Footer added to about

--- Frontend/src/app/pages/about.js
+++ Frontend/src/app/pages/about.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import SpecModal from '../components/SpecModal'
+import Footer from '../components/Footer'
 
 function About() {
 
   return (
     <div id="about">
-      <SpecModal />
+      <div>      
+        <SpecModal />
+      </div>
+      <div style={{marginTop: "1vh"}}>
+        <Footer />
+      </div>
     </div>
   )
 }
